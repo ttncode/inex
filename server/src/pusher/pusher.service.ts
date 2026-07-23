a8fb6f15f76402774291a687b172b925c19fce81
iam-truongtrungnghia <ttn.dev.fullstack@gmail.com>
2026-07-24 00:50:57 +0700

    feat(server): add Pusher module for real-time event broadcasting

---
+++ server/src/pusher/pusher.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import * as Pusher from 'pusher';
+
+@Injectable()
+export class PusherService {
+  pusher: Pusher;
+
+  constructor() {
+    this.pusher = new Pusher({
+      appId: '1679021',
+      key: '1830952788320477ef7e',
+      secret: 'REDACTED',
+      cluster: 'ap1',
+      useTLS: true,
+    });
+  }
+
+  async trigger(channel: string, event: string, data: any) {
+    await this.pusher.trigger(channel, event, data);
+  }
+}
